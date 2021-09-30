836997ebbded965d6737567a6b00670b2c9c0e61
Nikita Hiwse <hiwsenikita04@gmail.com>
2021-09-30 12:30:59 +0530

    Create sum.java

---
+++ src/test/java/demo/sum.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10;
+		int b=5;
+		int c;
+		c=a+b;
+		System.out.println("addition of numbers is "+ c); 
+	}
+
+}
