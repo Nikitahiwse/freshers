aa2f84c8302290416048fe89ae1c528ecab70cef
Nikita Hiwse <hiwsenikita04@gmail.com>
2021-09-30 12:21:55 +0530

    Create sub.java

---
+++ src/test/java/demo/sub.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a=10;
+		int b=5;
+		int c;
+		c=a-b;
+		System.out.println("substractions is "+ c);
+		
+	}
+
+}
